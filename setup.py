7a3c0e7dfbe8b4ee47e5480abde9ce2db9b11389
Amir <nerd2156@gmail.com>
2025-07-04 20:53:59 +0330

    Project added to git

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup, find_packages  # type: ignore
+
+# خواندن پیش‌نیازها از فایل requirements.txt
+try:
+    with open("requirements.txt", "r", encoding="utf-8") as f:
+        requirements = f.read().splitlines()
+except FileNotFoundError:
+    requirements = []
+
+setup(
+    name="todo_app",
+    version="0.1.0",
+    author="AmirAsadyan",
+    description="A simple To-Do list application with Tkinter.",
+    packages=find_packages(),
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+            # مسیر جدید به تابع main را مشخص می‌کنیم
+            "todo=todo_app.app:main",
+        ],
+    },
+    python_requires=">=3.6",
+)
